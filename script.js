526dbb874de293c2e2c486d274887f399bcc3f0b
EC2 Default User <ec2-user@ip-172-31-46-74.us-east-2.compute.internal>
2022-05-07 23:41:16 +0000

    14

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+
 const GENERATION_HASH = '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6'
 const EPOCH = 1615853185
 const XYM_ID = '6BED913FA20223F8'
 const NODE_URL = 'https://sym-main.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.MAIN_NET
+
 
 const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
 const accountHttp = repositoryFactory.createAccountRepository()
@@ -23,7 +25,7 @@ accountHttp.getAccountInfo(address)
     for (let m of accountInfo.mosaics) {
       if (m.id.id.toHex() === XYM_ID) {
         const dom_xym = document.getElementById('wallet-xym')
-        dom_xym.innerText = `TEMPLE point : ${m.amount.compact() / Math.pow(10, 6)}`
+        dom_xym.innerText = `XYM 残高 : ${m.amount.compact() / Math.pow(10, 6)}`
       }
     }
   })
@@ -90,103 +92,4 @@ function handleSSS() {
     console.log('signedTx', signedTx)
     transactionHttp.announce(signedTx)
   })
-}
-
-{
-    "network": {
-        "identifier": "mainnet",
-        "nemesisSignerPublicKey": "BE0B4CF546B7B4F4BBFCFF9F574FDA527C07A53D3FC76F8BB7DB746F8E8E0A9F",
-        "nodeEqualityStrategy": "host",
-        "generationHashSeed": "57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6",
-        "epochAdjustment": "1615853185s"
-    },
-    "chain": {
-        "enableVerifiableState": true,
-        "enableVerifiableReceipts": true,
-        "currencyMosaicId": "0x6BED'913F'A202'23F8",
-        "harvestingMosaicId": "0x6BED'913F'A202'23F8",
-        "blockGenerationTargetTime": "30s",
-        "blockTimeSmoothingFactor": "3000",
-        "importanceGrouping": "720",
-        "importanceActivityPercentage": "5",
-        "maxRollbackBlocks": "0",
-        "maxDifficultyBlocks": "60",
-        "defaultDynamicFeeMultiplier": "100",
-        "maxTransactionLifetime": "6h",
-        "maxBlockFutureTime": "300ms",
-        "initialCurrencyAtomicUnits": "7'842'928'625'000'000",
-        "maxMosaicAtomicUnits": "8'999'999'999'000'000",
-        "totalChainImportance": "7'842'928'625'000'000",
-        "minHarvesterBalance": "10'000'000'000",
-        "maxHarvesterBalance": "50'000'000'000'000",
-        "minVoterBalance": "3'000'000'000'000",
-        "votingSetGrouping": "1440",
-        "maxVotingKeysPerAccount": "3",
-        "minVotingKeyLifetime": "112",
-        "maxVotingKeyLifetime": "360",
-        "harvestBeneficiaryPercentage": "25",
-        "harvestNetworkPercentage": "5",
-        "harvestNetworkFeeSinkAddressV1": "NBUTOBVT5JQDCV6UEPCPFHWWOAOPOCLA5AY5FLI",
-        "harvestNetworkFeeSinkAddress": "NAVORTEX3IPBAUWQBBI3I3BDIOS4AVHPZLCFC7Y",
-        "maxTransactionsPerBlock": "6'000"
-    },
-    "plugins": {
-        "accountlink": {
-            "dummy": "to trigger plugin load"
-        },
-        "aggregate": {
-            "maxTransactionsPerAggregate": "100",
-            "maxCosignaturesPerAggregate": "25",
-            "enableStrictCosignatureCheck": false,
-            "enableBondedAggregateSupport": true,
-            "maxBondedTransactionLifetime": "48h"
-        },
-        "lockhash": {
-            "lockedFundsPerAggregate": "10'000'000",
-            "maxHashLockDuration": "2d"
-        },
-        "locksecret": {
-            "maxSecretLockDuration": "365d",
-            "minProofSize": "0",
-            "maxProofSize": "1024"
-        },
-        "metadata": {
-            "maxValueSize": "1024"
-        },
-        "mosaic": {
-            "maxMosaicsPerAccount": "1'000",
-            "maxMosaicDuration": "3650d",
-            "maxMosaicDivisibility": "6",
-            "mosaicRentalFeeSinkAddressV1": "NC733XE7DF46Q7QYLIIZBBSCJN2BEEP5FQ6PAYA",
-            "mosaicRentalFeeSinkAddress": "NCVORTEX4XD5IQASZQEHDWUXT33XBOTBMKFDCLI",
-            "mosaicRentalFee": "500000"
-        },
-        "multisig": {
-            "maxMultisigDepth": "3",
-            "maxCosignatoriesPerAccount": "25",
-            "maxCosignedAccountsPerAccount": "25"
-        },
-        "namespace": {
-            "maxNameSize": "64",
-            "maxChildNamespaces": "100",
-            "maxNamespaceDepth": "3",
-            "minNamespaceDuration": "30d",
-            "maxNamespaceDuration": "1825d",
-            "namespaceGracePeriodDuration": "30d",
-            "reservedRootNamespaceNames": "symbol, symbl, xym, xem, nem, user, account, org, com, biz, net, edu, mil, gov, info",
-            "namespaceRentalFeeSinkAddressV1": "NBDTBUD6R32ZYJWDEWLJM4YMOX3OOILHGDUMTSA",
-            "namespaceRentalFeeSinkAddress": "NCVORTEX4XD5IQASZQEHDWUXT33XBOTBMKFDCLI",
-            "rootNamespaceRentalFeePerBlock": "2",
-            "childNamespaceRentalFee": "100000"
-        },
-        "restrictionaccount": {
-            "maxAccountRestrictionValues": "100"
-        },
-        "restrictionmosaic": {
-            "maxMosaicRestrictionValues": "20"
-        },
-        "transfer": {
-            "maxMessageSize": "1024"
-        }
-    }
 }
